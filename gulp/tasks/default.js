cc88958b9ba6cd057408eecdef0c8497ad1f2f86
just4fun <houritsunohikari@gmail.com>
2014-12-23 18:24:25 +0800

    add basic infrastructure

---
+++ gulp/tasks/default.js
@@ -0,0 +1,9 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch([
+  	'./index.html',
+  	'./js/**'
+  ], browserSync.reload);
+});
