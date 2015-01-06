e5bd9cb195002b64902f90ab2a34aa99a553a7ed
just4fun <houritsunohikari@gmail.com>
2015-01-06 23:01:23 +0800

    search for fun

--- js/collections/books.js
+++ js/collections/books.js
@@ -8,9 +8,11 @@ define([
 
         model: Book,
 
-        url: 'https://api.douban.com/v2/book/search?q=%E6%9D%91%E4%B8%8A%E6%98%A5%E6%A0%91',
+        url: function () {
+          return 'https://api.douban.com/v2/book/search?q=' + this.param;
+        },
 
-        sync : function(method, collection, options) {
+        sync: function(method, collection, options) {
             options.dataType = "jsonp";
             return Backbone.sync(method, collection, options);
         }
--- js/views/home.js
+++ js/views/home.js
@@ -4,29 +4,53 @@ define([
   'backbone',
   'base/backbone.baseview',
   'collections/books',
-  'text!templates/home.html',
-], function ($, _, Backbone, BackboneBaseView, Books, homeTemp) {
+  'text!templates/home_search.html',
+  'text!templates/home_result.html'
+], function ($, _, Backbone, BackboneBaseView, Books, homeSearchTemp, homeResultTemp) {
 
   var HomeView = BackboneBaseView.extend({
 
-    template: _.template(homeTemp),
-
     events: {
-
+      "click #btn_search": "search",
+      "keyup #txt_search": "keyPress"
     },
 
     initialize: function (options) {
       this.$el = options.$el;
-      this.listenTo(Books, 'reset', this.render);
-
-      $('.loadingbox').show();
-      Books.fetch({reset: true});
+      this.listenTo(Books, 'reset', this.subRender);
+      this.render();
     },
 
     render: function () {
-      this.$el.html(this.template({books: Books.toJSON()[0].books}));
-      $('.loadingbox').hide();
+      this.$el.html(homeSearchTemp);
       return this;
+    },
+
+    subRender: function () {
+      var list = Books.toJSON()[0].books;
+      var homeTemp = _.template(homeResultTemp);
+      var renderedTemp = homeTemp({books: list});
+      this.$el.children('#search_list').remove();
+      this.$el.append(renderedTemp);
+      $('.loadingbox').hide();
+    },
+
+    search: function () {
+      var keyword = $('#txt_search').val().trim();
+      if (keyword == "") {
+        $('#txt_search').val();
+        return;
+      }
+
+      $('.loadingbox').show();
+      Books.param = keyword;
+      Books.fetch({reset: true});
+    },
+
+    keyPress: function (event) {
+      if (event.keyCode == 13){
+        this.search();
+      }
     }
 
   });
