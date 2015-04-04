d6e539265df038f2c74f7966785e09b78862d0aa
just4fun <houritsunohikari@gmail.com>
2015-04-04 14:54:28 +0800

    add dev environment for react_flux

---
+++ repos/react_flux/gulp/tasks/browserify.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var bundleLogger = require('../utils/bundleLogger');
+
+var path = {
+  ENTRY_POINT: './js/app.js',
+  OUTPUT: 'bundle.js',
+  DEST: './dest'
+};
+
+gulp.task('browserify', function() {
+  var b = browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: false
+  });
+
+  var bundle = function () {
+    bundleLogger.start(path.OUTPUT);
+
+    return b.bundle()
+      .pipe(source(path.OUTPUT))
+      .pipe(gulp.dest(path.DEST))
+      .pipe(browserSync.reload({stream: true}));
+  };
+
+  b = watchify(b);
+  b.on('update', bundle);
+  bundleLogger.watch(path.OUTPUT);
+
+  return bundle();
+});
