fdc57efa7e3ba1e1fc0b02094f1dd108f1e3a856
just4fun <houritsunohikari@gmail.com>
2015-04-23 22:46:30 +0800

    beautify markup of book list

--- repos/react_flux/js/components/BookItem.react.js
+++ repos/react_flux/js/components/BookItem.react.js
@@ -5,13 +5,32 @@ var Link = Router.Link;
 var BookItem = React.createClass({
 
   render: function() {
+    var subtitle = this.props.book.subtitle;
+    if (subtitle) {
+      subtitle = " : " + subtitle;
+    }
+    var authors = this.props.book.author.join('、');
+    var publisher = this.props.book.publisher;
+    if (publisher) {
+      publisher = " / " + publisher;
+    }
+    var pubdate = this.props.book.pubdate;
+    if (pubdate) {
+      pubdate = " / " + pubdate;
+    }
+    var price = this.props.book.price;
+    if (price) {
+      price = " / " + price;
+    }
+
     return (
       <Link to='book-detail' params={{bookId: this.props.book.id}} className='main-section__book clearfix'>
         <div className='pull-left'>
           <img className='main-section__book-img' src={this.props.book.image}/>
         </div>
-        <div className='main-section__book-info pull-left'>
-          <span className='main-section__book-title'>{this.props.book.title}</span>
+        <div className='main-section__book-info'>
+        <div className='main-section__book-title'>{this.props.book.title}<small>{subtitle}</small></div>
+          <div>{authors}{publisher}{pubdate}{price}</div>
         </div>
       </Link>
     );
