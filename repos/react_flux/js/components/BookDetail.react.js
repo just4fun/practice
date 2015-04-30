fdf3113ea63bf0b369a4a343d4345c03dcb7d27c
just4fun <houritsunohikari@gmail.com>
2015-04-30 17:23:16 +0800

    improve book detail page

--- repos/react_flux/js/components/BookDetail.react.js
+++ repos/react_flux/js/components/BookDetail.react.js
@@ -29,8 +29,63 @@ var BookDetail = React.createClass({
     if (this.state.isLoading) {
       return <Loading />;
     }
+    var book = this.state.book;
+    var authors = book.author;
+    if (authors) {
+      authors = "作者：" + authors.join(' / ')
+    }
+    var publisher = book.publisher;
+    if (publisher) {
+      publisher = "出版社：" + publisher;
+    }
+    var pubdate = book.pubdate;
+    if (pubdate) {
+      pubdate = "出版年：" + pubdate;
+    }
+    var pages = book.pages;
+    if (pages) {
+      pages = "页数：" + pages;
+    }
+    var price = book.price;
+    if (price) {
+      price = "定价：" + price;
+    }
+    var summary = book.summary;
+    if (summary) {
+      summary =
+        <div>
+          <h4>内容简介</h4>
+          <p>{book.summary}</p>
+        </div>
+    }
+    var author_intro = book.author_intro;
+    if (author_intro) {
+      author_intro =
+        <div>
+          <h4>作者简介</h4>
+          <p>{book.author_intro}</p>
+        </div>
+    }
     return (
-      <div>{this.state.book.summary}</div>
+      <div className='book-detail'>
+        <div>
+          <h3>{book.title}</h3>
+          <div className='clearfix'>
+            <div className='pull-left'>
+              <img className='book-detail--img' src={book.image}/>
+            </div>
+            <div className='book-detail--info'>
+              <div>{authors}</div>
+              <div>{publisher}</div>
+              <div>{pubdate}</div>
+              <div>{pages}</div>
+              <div>{price}</div>
+            </div>
+          </div>
+        </div>
+        {summary}
+        {author_intro}
+      </div>
     );
   },
 
